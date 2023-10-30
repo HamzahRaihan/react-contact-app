bf6e1ba40ecbf7a70d59feaa3f93a1675456e1c2
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-10-30 18:07:03 +0700

    added feature to create new contact

--- src/pages/ContactApp.jsx
+++ src/pages/ContactApp.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ContactList from '../components/ContactList';
 import RatingStars from '../components/RatingStars';
 import { getData } from '../utils/datas';
+import ContactInput from '../components/ContachInput';
 
 class ContactApp extends React.Component {
   constructor(props) {
@@ -10,15 +11,35 @@ class ContactApp extends React.Component {
       contacts: getData(),
     };
     this.onDeleteHandler = this.onDeleteHandler.bind(this);
+    this.onAddContactHandler = this.onAddContactHandler.bind(this);
   }
+
   onDeleteHandler(id) {
     const contacts = this.state.contacts.filter((contact) => contact.id !== id);
     this.setState({ contacts });
   }
 
+  onAddContactHandler({ name, tag }) {
+    this.setState((prevState) => {
+      return {
+        contacts: [
+          ...prevState.contacts,
+          {
+            id: +new Date(),
+            name,
+            tag,
+            imageUrl: '/images/default.jpg',
+          },
+        ],
+      };
+    });
+  }
+
   render() {
     return (
       <div className="contact-app">
+        <h1>Aplikasi Kontak</h1>
+        <ContactInput addContact={this.onAddContactHandler} />
         <h1>Daftar Kontak</h1>
         <ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandler} />
         <RatingStars />
